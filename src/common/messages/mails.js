78d3ade2ac29f185397c487238c6140a23a20c54
Martin Litvaj <kamahl19@gmail.com>
2019-06-17 20:46:38 +0200

    fix reset password route

--- src/common/messages/mails.js
+++ src/common/messages/mails.js
@@ -5,7 +5,7 @@ module.exports = {
     subject: 'Reset your password',
     text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n
   Please click on the following link, or paste this into your browser to complete the process:\n
-  ${origin}/reset-password/${passwordResetToken}\n
+  ${origin}/auth/reset-password/${passwordResetToken}\n
   If you did not request this, please ignore this email and your password will remain unchanged.\n`,
   }),
 
